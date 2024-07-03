517b88037b7c53f19b4b06446a1f58ecab3f4845
Molina-Q <quentinmolina1@gmail.com>
2024-07-03 16:43:27 +0200

    added select and terms fields

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,13 +1,29 @@
 <template>
     <form>
         <label for="email">Email:</label>
-        <input v-model="email" type="email" required>
+        <input v-model="email" type="email" id="email" required>
 
-        <label>Password:</label>
-        <input v-model="password" type="password" required>
+        <label for="password">Password:</label>
+        <input v-model="password" type="password" id="password" required>
+
+        <label for="role">Role:</label>
+        <select id="role" v-model="role">
+            <option value="">-- None --</option>
+            <option value="developer">Web Developer</option>
+            <option value="designer">Web Designer</option>
+        </select>
+
+        <div class="terms">
+            <input v-model="terms" type="checkbox" required>
+            <label>Accept terms and conditions</label>
+        </div>
+
     </form>
+
     <p>Email: {{ email }}</p>
     <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+    <p>Terms accepted: {{ terms ? 'Yes' : 'No' }}</p>
 </template>
 
 <script>
@@ -15,7 +31,9 @@ export default {
     data() {
         return {
             email: '',
-            password: ''
+            password: '',
+            role: '',
+            terms: false,
         }
     }
 }
@@ -41,7 +59,8 @@ label {
     font-weight: bold;
 }
 
-input {
+input,
+select {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -49,5 +68,13 @@ input {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
 }
 </style>
